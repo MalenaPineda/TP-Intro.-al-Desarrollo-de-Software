f41bdff2ce9080ef47b3d72c3183e7b17b2fdbf0
Eugenia Brum <mbrum@fi.uba.ar>
2026-06-21 18:23:40 -0300

    feat: agregar JOIN con tabla usuarios en query de gastos

--- app/src/backend/api/gastos.js
+++ app/src/backend/api/gastos.js
@@ -4,7 +4,7 @@ import { db } from "../../../db/pool.js";
 export const endpointsGastos = Router();
 
 export async function getGastos() {
-  const result = await db.query("SELECT * FROM gastos");
+  const result = await db.query("SELECT g.id_gasto,g.descripcion,g.monto,g.fecha_gasto,g.metodo_pago,g.categoria,u.id_user,u.nombre FROM gastos g, usuarios u WHERE g.id_user = u.id_user ORDER BY g.fecha_gasto DESC; ")
   return result.rows;
 }
 
@@ -12,6 +12,7 @@ endpointsGastos.get("/", async (req, res) => {
   try {
     const gastos = await getGastos();
     res.json(gastos);
+
   } catch (error) {
     console.error("Error al obtener los gastos:", error);
     res.status(500).json({ error: "Error interno del servidor" });
