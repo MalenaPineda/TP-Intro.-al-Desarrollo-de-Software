ea2b538f5fa28893e2307f7080f16d3afea979c4
Eugenia Brum <mbrum@fi.uba.ar>
2026-06-22 18:41:02 -0300

    Agrego la función getTotalGastoPorMes para reportes mensuales sobre los gastos

--- app/db/gastos.js
+++ app/db/gastos.js
@@ -4,3 +4,8 @@ export async function getGastos() {
     const result = await db.query("SELECT g.id_gasto,g.descripcion,g.monto,g.fecha_gasto,g.metodo_pago,g.categoria,u.id_user,u.nombre FROM gastos g, usuarios u WHERE g.id_user = u.id_user ORDER BY g.fecha_gasto DESC; ")
     return result.rows;
   }
+
+export async function getTotalGastoPorMes() {
+    const result = await db.query("SELECT SUM(monto) AS total FROM gastos WHERE EXTRACT(MONTH FROM fecha_gasto) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM fecha_gasto) = EXTRACT(YEAR FROM CURRENT_DATE);")
+    return result.rows;
+  }
