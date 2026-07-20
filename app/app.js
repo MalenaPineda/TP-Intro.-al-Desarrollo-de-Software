d7a88d340c8edf3debfe12c7e1dceb37a82f8b9c
Jesus David Guerra <jguerrap@fi.uba.ar>
2026-07-20 12:58:08 -0300

    Arreglo conflictos varios en docker-compose, app.js, schemas.sql

--- app/app.js
+++ app/app.js
@@ -2,10 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import { endpointsGastos } from "./src/backend/api/gastos.js";
-<<<<<<< HEAD
 import { rutaTareas } from "./src/backend/api/tareas.js";
-=======
->>>>>>> d822528838800fe9fefae368d9468efe047300a0
 
 const app = express();
 const port = 8000;
@@ -14,10 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use("/api/v1/gastos", endpointsGastos);
-<<<<<<< HEAD
 app.use("/api/v1/tareas", rutaTareas);
-=======
->>>>>>> d822528838800fe9fefae368d9468efe047300a0
 
 app.get("/health", (req, res) => {
   res.send("OK");
