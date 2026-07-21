c4ba61fcbb460e00d61d313ffcac9a90256ba35b
Eugenia Brum <mbrum@fi.uba.ar>
2026-07-21 17:28:16 -0300

    feat: creo el endpoint que permite que se modifiquen los datos de la base de datos

--- app/db/gastos.js
+++ app/db/gastos.js
@@ -52,3 +52,24 @@ export async function getMetodoPago() {
   return result.rows;
 }
 
+export async function getGastosPorMes() {
+  const result = await db.query(`
+    SELECT 
+      EXTRACT(MONTH FROM fecha_gasto) AS mes,
+      EXTRACT(YEAR FROM fecha_gasto) AS anio,
+      SUM(monto) AS total
+    FROM gastos
+    GROUP BY anio, mes
+    ORDER BY anio ASC, mes ASC
+  `);
+  return result.rows;
+  
+}
+
+export async function updateGasto(id, descripcion, monto, metodo_pago, id_categoria) {
+  const result = await db.query(
+    "UPDATE gastos SET descripcion = $1, monto = $2, metodo_pago = $3, categoria = $4 WHERE id_gasto = $5",
+    [descripcion, monto, metodo_pago, id_categoria, id]
+  );
+  return result.rowCount > 0;
+}
