066b26c14b05519f7a806c9dbd0d3f192c95c57f
Eugenia Brum <mbrum@fi.uba.ar>
2026-07-21 17:29:08 -0300

    feat: creo el endpoint que permite que se modifiquen los datos de la base de datos

--- app/src/backend/api/gastos.js
+++ app/src/backend/api/gastos.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getGastos, getTotalGastosPorUsuario, getTotalGastoPorMes, getGastosPorCategoria, createGasto, getNombreCategoria, getMetodoPago } from "../../../db/gastos.js";
+import { getGastos, getTotalGastosPorUsuario, getTotalGastoPorMes, getGastosPorCategoria, createGasto, getNombreCategoria, getMetodoPago, updateGasto } from "../../../db/gastos.js";
 
 export const endpointsGastos = Router();
 
@@ -102,6 +102,23 @@ endpointsGastos.get("/metodo-pago", async (req, res) => {
     res.json(metodos);
   } catch (error) {
     console.error("Error al obtener las categorias:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+endpointsGastos.put("/:id", async (req, res) => {
+  console.log(req)
+  try {
+    const id = req.params.id;
+    const { descripcion, monto, metodo_pago, categoria } = req.body;
+    const updated = await updateGasto(id, descripcion, monto, metodo_pago, categoria);
+    if (!updated) {
+      res.sendStatus(404);
+      return;
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error al actualizar el gasto:", error);
     res.status(500).json({ error: "Error interno del servidor" });
   }
 });
