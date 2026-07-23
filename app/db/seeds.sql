6427da111ea41d10a6b98ff3767285337ba075c6
Eugenia Brum <mbrum@fi.uba.ar>
2026-07-23 19:11:16 -0300

    refactor: modifico el insert par ingreso de datos para que ingrese gstos en el mes de julio

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -20,12 +20,12 @@ INSERT INTO categoria_gastos (nombre) VALUES
 ('Artículos de Limpieza');
 
 -- Métodos de pago
-INSERT INTO metodo_pago (nombre) VALUES 
+INSERT INTO metodo_pago VALUES 
 ('Efectivo'),
 ('Tarjeta de crédito'),
 ('Transferencia');
 
 -- Gastos
 INSERT INTO gastos (descripcion, monto, fecha_gasto, id_metodo, categoria, id_user) VALUES 
-('Compra mensual Coto', 45000.00, '2026-06-10', 2, 2, 1),
-('Factura de Edesur', 12500.50, '2026-06-12', 3, 3, 2);
+('Compra mensual Coto', 45000.00, '2026-07-10', 2, 2, 1),
+('Factura de Edesur', 12500.50, '2026-07-12', 3, 3, 2);
