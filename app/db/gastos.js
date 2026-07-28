23a1f483cc8bde602145fb4d0f6ceaced255a4e4
Eugenia Brum <mbrum@fi.uba.ar>
2026-07-28 18:30:07 -0300

    fix: arreglo la consulta sql getGastos

--- app/db/gastos.js
+++ app/db/gastos.js
@@ -2,20 +2,20 @@ import { db } from "./pool.js";
 
 export async function getGastos() {
   const result = await db.query(`
-    SELECT 
-      g.id_gasto,
-      g.descripcion,
-      g.monto,
-      g.fecha_gasto,
-      g.categoria,
-      u.id_user,
-      u.nombre,
-      m.nombre AS metodo_pago
-    FROM gastos g, usuarios u, metodo_pago m
-    WHERE g.id_user = u.id_user
-    AND g.id_metodo = m.id
-    ORDER BY g.fecha_gasto DESC
-  `)
+  SELECT 
+  g.id_gasto,
+  g.descripcion,
+  g.monto,
+  g.fecha_gasto,
+  g.categoria,
+  u.id_user,
+  u.nombre,
+  m.nombre AS metodo_pago
+FROM gastos g, usuarios u, metodo_pago m
+WHERE g.id_user = u.id_user
+AND g.id_metodo = m.id
+ORDER BY g.fecha_gasto DESC
+`)
   return result.rows;
 }
 
