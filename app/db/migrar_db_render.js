6eac98de08dcc22b94107248da246b0aa20ac831
Eugenia Brum <mbrum@fi.uba.ar>
2026-08-03 20:01:32 -0300

    feat: agrego a migrar_db_render.js para que ejecute los archivos delete.sql para eliminar datos de la base de datos, schemas.sql para crear las tablas y seed.sql para que agregue datos

--- app/db/migrar_db_render.js
+++ app/db/migrar_db_render.js
@@ -13,9 +13,10 @@ async function run(file) {
 }
 
 async function main() {
-    await run("./schemas.sql");
-    await run("./seed.sql");
-    await pool.end();
+  await run("./delete.sql");     // Borra todo
+  await run("./schemas.sql");    // Crea tablas de nuevo
+  await run("./seeds.sql");       // Inserta datos nuevos
+  await pool.end();
   }
 main().catch((err) => {
   console.error("Error:", err);
