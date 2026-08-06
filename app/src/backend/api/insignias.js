78f08deb7c60a6d5d2c9705bcba79a56a1582a2c
Eugenia Brum <mbrum@fi.uba.ar>
2026-08-06 17:00:33 -0300

    feat: agrego endpoint que trae los iconos

--- app/src/backend/api/insignias.js
+++ app/src/backend/api/insignias.js
@@ -1,57 +1,64 @@
 import { Router } from 'express';
 
-import { getInsignias, createInsignia, updateInsignia, deleteInsignia } from "../../../db/insignias.js";
+import { getInsignias, createInsignia, updateInsignia, deleteInsignia, getIconos } from "../../../db/insignias.js";
 export const endpointsInsignias = Router();
 
 endpointsInsignias.get("/", async (req, res) => {
-    try {
-      const insignias = await getInsignias();
-      res.json(insignias);
-    } catch (error) {
-      console.error("Error al obtener las insigneas:", error.message);
-      res.status(500).json({ error: "Error interno del servidor" });
-    }
-  });
+  try {
+    const insignias = await getInsignias();
+    res.json(insignias);
+  } catch (error) {
+    console.error("Error al obtener las insigneas:", error.message);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
 
+endpointsInsignias.get("/iconos", async (req, res) => {
+  try {
+    const iconos = await getIconos();
+    res.json(iconos);
+  } catch (error) {
+    console.error("Error al obtener iconos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
 
-  endpointsInsignias.post("/", async (req, res) => {
-    const { nombre, descripcion, cant_tarea, id_categoria_tarea, icono } = req.body;
+endpointsInsignias.post("/", async (req, res) => {
+  const { nombre, descripcion, cant_tarea, id_categoria_tarea, icono } = req.body;
 
-    if (!nombre || !descripcion || !cant_tarea) {
-      res.status(400).send("Faltan campos obligatorios");
+  if (!nombre || !descripcion || !cant_tarea) {
+    res.status(400).send("Faltan campos obligatorios");
+    return;
+  }
+  try {
+    const insignia = await createInsignia(nombre, descripcion, cant_tarea, id_categoria_tarea, icono);
+    res.status(201).json(insignia);
+  } catch (error) {
+    console.error("Error al crear insignia:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+endpointsInsignias.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleted = await deleteInsignia(id);
+    if (!deleted) {
+      res.sendStatus(404);
       return;
     }
-  
-    try {
-    const updated = await updateInsignia(id, nombre, descripcion, cant_tarea, id_categoria_tarea, icono);      res.status(201).json(insignia);
-    } catch (error) {
-      console.error("Error al crear insignia:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
-    }
-  });
-
-
-  endpointsInsignias.delete("/:id", async (req, res) => {
-    try {
-      const id = req.params.id;
-      const deleted = await deleteInsignia(id);
-      if (!deleted) {
-        res.sendStatus(404);
-        return;
-      }
-      res.sendStatus(200);
-    } catch (error) {
-      console.error("Error al borrar insignia:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
-    }
-  });
-  
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error al borrar insignia:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
 
 endpointsInsignias.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const { nombre, descripcion, cant_tarea, id_categoria_tarea } = req.body;
-    const updated = await updateInsignia(id, nombre, descripcion, cant_tarea, id_categoria_tarea);
+    const { nombre, descripcion, cant_tarea, id_categoria_tarea, icono } = req.body;
+    const updated = await updateInsignia(id, nombre, descripcion, cant_tarea, id_categoria_tarea, icono);
     if (!updated) {
       res.sendStatus(404);
       return;
@@ -61,4 +68,6 @@ endpointsInsignias.put("/:id", async (req, res) => {
     console.error("Error al actualizar insignia:", error);
     res.status(500).json({ error: "Error interno del servidor" });
   }
-});  
+});
+
+export default endpointsInsignias;
