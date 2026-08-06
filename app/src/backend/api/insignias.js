3ce14f6a7aa2cd441a151b4f1c5aa8bb0e4e0776
Eugenia Brum <mbrum@fi.uba.ar>
2026-08-05 22:11:14 -0300

    feat: agrego los endpoints  completo que permite borrar, crear y editar insignias

--- app/src/backend/api/insignias.js
+++ app/src/backend/api/insignias.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { getInsignias } from "../../../db/insignias.js";
+import { getInsignias, createInsignia, updateInsignia, deleteInsignia } from "../../../db/insignias.js";
 export const endpointsInsignias = Router();
 
 endpointsInsignias.get("/", async (req, res) => {
@@ -12,3 +12,53 @@ endpointsInsignias.get("/", async (req, res) => {
       res.status(500).json({ error: "Error interno del servidor" });
     }
   });
+
+
+  endpointsInsignias.post("/", async (req, res) => {
+    const { nombre, descripcion, cant_tarea, id_categoria_tarea, icono } = req.body;
+
+    if (!nombre || !descripcion || !cant_tarea) {
+      res.status(400).send("Faltan campos obligatorios");
+      return;
+    }
+  
+    try {
+    const updated = await updateInsignia(id, nombre, descripcion, cant_tarea, id_categoria_tarea, icono);      res.status(201).json(insignia);
+    } catch (error) {
+      console.error("Error al crear insignia:", error);
+      res.status(500).json({ error: "Error interno del servidor" });
+    }
+  });
+
+
+  endpointsInsignias.delete("/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const deleted = await deleteInsignia(id);
+      if (!deleted) {
+        res.sendStatus(404);
+        return;
+      }
+      res.sendStatus(200);
+    } catch (error) {
+      console.error("Error al borrar insignia:", error);
+      res.status(500).json({ error: "Error interno del servidor" });
+    }
+  });
+  
+
+endpointsInsignias.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { nombre, descripcion, cant_tarea, id_categoria_tarea } = req.body;
+    const updated = await updateInsignia(id, nombre, descripcion, cant_tarea, id_categoria_tarea);
+    if (!updated) {
+      res.sendStatus(404);
+      return;
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error al actualizar insignia:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});  
