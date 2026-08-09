3848c64dee21aa43555105195a93418dfa23ec72
Malena Pineda <mpineda@fi.uba.ar>
2026-08-09 18:29:53 -0300

    validar fecha de nacimiento y ocultar contrasenia (#49)
    Apruebo PR

--- app/db/usuarios.js
+++ app/db/usuarios.js
@@ -20,18 +20,19 @@ export async function getUsuarioPorId(id) {
 
 //Crear usuario
 export async function crearUsuario(nombre, email, contrasenia, fecha_nacimiento) {
-    const resultado = await db.query(`INSERT INTO usuarios (nombre, email, contrasenia,fecha_nacimiento) VALUES ($1, $2, $3, $4) RETURNING *`,[nombre,email,contrasenia,fecha_nacimiento]);
+    const resultado = await db.query(`INSERT INTO usuarios (nombre, email, contrasenia,fecha_nacimiento) VALUES ($1, $2, $3, $4) RETURNING *`, [nombre, email, contrasenia, fecha_nacimiento]);
     return resultado.rows[0];
+
 }
 
-export async function editarUsuario(id,nombre,email,contrasenia,fecha_nacimiento) {
-    const resultado = await db.query(`UPDATE usuarios SET nombre = $1, email = $2, contrasenia = $3, fecha_nacimiento = $4 WHERE id_user = $5 RETURNING *`, [nombre,email,contrasenia,fecha_nacimiento,id]);
+export async function editarUsuario(id, nombre, email, contrasenia, fecha_nacimiento) {
+    const resultado = await db.query(`UPDATE usuarios SET nombre = $1, email = $2, contrasenia = $3, fecha_nacimiento = $4 WHERE id_user = $5 RETURNING *`, [nombre, email, contrasenia, fecha_nacimiento, id]);
     return resultado.rows[0];
 }
 
 //En este caso hacemos soft delete, simpleente cambiamos su estado activo para mantener sus tareas y gastos
 export async function borrarUsuario(id) {
-    const resultado = await db.query(`UPDATE usuarios SET activo = FALSE WHERE id_user = $1 RETURNING *`,[id]);
+    const resultado = await db.query(`UPDATE usuarios SET activo = FALSE WHERE id_user = $1 RETURNING *`, [id]);
     return resultado.rows[0];
 }
 
