1812f30758605649f892784b001d9c13fec50797
Eugenia Brum <mbrum@fi.uba.ar>
2026-08-11 19:44:13 -0300

    fix: cambio el nombre de la variale de URL_TAREAS a URL_API_TAREAS

--- app/src/frontend/js/dashboard.js
+++ app/src/frontend/js/dashboard.js
@@ -1,5 +1,5 @@
 const URL_API = `${window.location.origin}/api/v1/gastos`;
-const URL_TAREAS = `${window.location.origin}/api/v1/tareas`;
+const URL_API_TAREAS = `${window.location.origin}/api/v1/tareas`;
 
 
 const coloresPorCategoria = {
